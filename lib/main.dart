5f6510901915dcfc903d7b1f84ccf528f8cca757
LordOlumide <olumideolubosede@yahoo.com>
2022-05-17 19:59:18 +0100

    Device orientation changing prevented

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator_reboot/screens/input_page.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(BMICalculator());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(BMICalculator());
+}
 
 class BMICalculator extends StatelessWidget {
   @override
